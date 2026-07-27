21890f5a50cddd2ee5eb3f210d8fb448955a888c
Santiago <santyago3601@gmail.com>
2026-07-26 23:17:15 -0500

    kafka consumer

--- src/main/java/com/santiago/learning/projects/webscrapping/products/dto/request/ProductRequestDTO.java
+++ src/main/java/com/santiago/learning/projects/webscrapping/products/dto/request/ProductRequestDTO.java
@@ -1,6 +1,5 @@
 package com.santiago.learning.projects.webscrapping.products.dto.request;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductRequestDTO {
-    @NotEmpty
-    private Long id;
+    @NotBlank
+    private String id;
     @NotBlank
     private String name;
     @NotBlank
--- src/test/java/com/santiago/learning/projects/webscrapping/products/WebscrappingApplicationTests.java
+++ src/test/java/com/santiago/learning/projects/webscrapping/products/WebscrappingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.santiago.learning.projects.webscrapping;
+package com.santiago.learning.projects.webscrapping.products;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
